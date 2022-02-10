591ef4fe6661b67b11077c0287989d1d1a121949
Sandra <sandra@okaeri.cloud>
2022-02-10 01:46:37 +0100

    Implement OkaeriValidator methods

--- src/main/java/eu/okaeri/validator/Validator.java
+++ src/main/java/eu/okaeri/validator/Validator.java
@@ -4,6 +4,7 @@ import eu.okaeri.validator.provider.ValidationProvider;
 import lombok.NonNull;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.Parameter;
 import java.util.Set;
 
@@ -15,7 +16,13 @@ public interface Validator {
 
     Set<ConstraintViolation> validateProperty(@NonNull Object object, @NonNull String fieldName);
 
+    Set<ConstraintViolation> validateProperty(@NonNull Object object, @NonNull Field field);
+
     Set<ConstraintViolation> validatePropertyValue(@NonNull Class<?> type, @NonNull String fieldName, Object fieldValue);
 
+    Set<ConstraintViolation> validatePropertyValue(@NonNull Class<?> type, @NonNull Field field, Object fieldValue);
+
     Set<ConstraintViolation> validateParameters(@NonNull Parameter[] parameters, @NonNull Object[] values);
+
+    Set<ConstraintViolation> validateParameter(@NonNull Parameter parameter, @NonNull Object value);
 }
