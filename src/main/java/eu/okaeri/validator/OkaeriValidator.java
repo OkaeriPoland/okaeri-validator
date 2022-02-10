6326257958580b884a2a3ea98e785e44e0d0701c
PinkLolicorn <pinklolicorngithub@daffit.pl>
2022-02-10 01:46:37 +0100

    Implement OkaeriValidator methods

--- src/main/java/eu/okaeri/validator/OkaeriValidator.java
+++ src/main/java/eu/okaeri/validator/OkaeriValidator.java
@@ -4,24 +4,21 @@ package eu.okaeri.validator;
 import eu.okaeri.validator.policy.NullPolicy;
 import eu.okaeri.validator.provider.*;
 import lombok.NonNull;
+import lombok.SneakyThrows;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Parameter;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class OkaeriValidator implements Validator {
 
-    private final NullPolicy nullPolicy;
     private final Map<Class<? extends Annotation>, ValidationProvider<?>> validationProviders = new ConcurrentHashMap<>();
 
     protected OkaeriValidator(NullPolicy nullPolicy) {
-        this.nullPolicy = nullPolicy;
         this.register(new DecimalMaxProvider());
         this.register(new DecimalMinProvider());
         this.register(new MaxProvider());
@@ -29,7 +26,7 @@ public class OkaeriValidator implements Validator {
         this.register(new NegativeOrZeroProvider());
         this.register(new NegativeProvider());
         this.register(new NotBlankProvider());
-        this.register(new NotNullProvider(this.nullPolicy));
+        this.register(new NotNullProvider(nullPolicy));
         this.register(new PatternProvider());
         this.register(new PositiveOrZeroProvider());
         this.register(new PositiveProvider());
@@ -52,42 +49,53 @@ public class OkaeriValidator implements Validator {
 
     @Override
     public Set<ConstraintViolation> validate(@NonNull Object object) {
-        return Arrays.stream(object.getClass().getFields())
-            .flatMap(field -> this.validatePropertyValue(object.getClass(), field.getName(), this.fieldValueOrNull(object, field)).stream())
+        return Arrays.stream(object.getClass().getDeclaredFields())
+            .flatMap(field -> this.validateProperty(object, field).stream())
             .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 
     @Override
+    @SneakyThrows
     public Set<ConstraintViolation> validateProperty(@NonNull Object object, @NonNull String fieldName) {
-        return this.validatePropertyValue(object.getClass(), fieldName, this.fieldValueOrNull(object, fieldName));
+        return this.validateProperty(object, object.getClass().getDeclaredField(fieldName));
     }
 
     @Override
+    @SneakyThrows
+    public Set<ConstraintViolation> validateProperty(@NonNull Object object, @NonNull Field field) {
+        field.setAccessible(true);
+        Object value = field.get(object);
+        return this.validatePropertyValue(object.getClass(), field, value);
+    }
+
+    @Override
+    @SneakyThrows
     public Set<ConstraintViolation> validatePropertyValue(@NonNull Class<?> type, @NonNull String fieldName, Object fieldValue) {
-        return null; // TODO
+        return this.validatePropertyValue(type, type.getDeclaredField(fieldName), fieldValue);
+    }
+
+    @Override
+    public Set<ConstraintViolation> validatePropertyValue(@NonNull Class<?> type, @NonNull Field field, Object fieldValue) {
+        return this.validationProviders.values().stream()
+            .filter(provider -> provider.shouldValidate(field))
+            .flatMap(provider -> provider.validate(field, fieldValue).stream())
+            .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 
     @Override
     public Set<ConstraintViolation> validateParameters(@NonNull Parameter[] parameters, @NonNull Object[] values) {
-        return null; // TODO
+        return IntStream
+            .range(0, parameters.length)
+            .mapToObj(i -> this.validateParameter(parameters[i], values[i]))
+            .flatMap(Collection::stream)
+            .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 
-    private Object fieldValueOrNull(Object object, String fieldName) {
-        try {
-            Field field = object.getClass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            return field.get(object);
-        } catch (IllegalAccessException | NoSuchFieldException ignored) {
-            return null;
-        }
-    }
-
-    private Object fieldValueOrNull(Object object, Field field) {
-        try {
-            field.setAccessible(true);
-            return field.get(object);
-        } catch (IllegalAccessException ignored) {
-            return null;
-        }
+    @Override
+    public Set<ConstraintViolation> validateParameter(@NonNull Parameter parameter, @NonNull Object value) {
+        return this.validationProviders.values().stream()
+            .filter(provider -> provider.shouldValidate(parameter))
+            .flatMap(provider -> provider.validate(parameter, value).stream())
+            .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 }
